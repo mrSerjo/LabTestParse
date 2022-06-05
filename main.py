0021c1b8181c575f219e5d4c2816e49db26aea07
mrserjo <mrserjo@mail.ru>
2022-06-05 21:56:01 +0300

    hw

--- main.py
+++ main.py
@@ -16,8 +16,47 @@ def collect_data():
     # При использовании proxy, добавьте аргумент proxies=proxies в requests.get переменной response ниже
     response = requests.get(url='https://www.lifetime.plus/api/analysis2')
 
-    with open(f'info_{t_date}.json', 'w', encoding='utf-8') as file:
-        json.dump(response.json(), file, indent=4, ensure_ascii=False)
+    # Для данных в json файле
+    # with open(f'info_{t_date}.json', 'w', encoding='utf-8') as file:
+    #     json.dump(response.json(), file, indent=4, ensure_ascii=False)
+
+
+    categories = response.json()['categories']
+    result = []
+
+    for c in categories:
+        c_name = c.get('name').strip()
+        c_items = c.get('items')
+
+        for item in c_items:
+            item_name = item.get('name').strip()
+            item_price = item.get('price')
+            item_desc = item.get('description').strip()
+
+        item_wt = item.get('days')
+        item_bio = item.get('biomaterial')
+
+        result.append(
+            [c_name, item_name, item_bio, item_desc, item_price, item_wt]
+        )
+
+    with open(f'result_{t_date}.csv', 'a', encoding='utf-8') as file:
+        writer = csv.writer(file)
+
+        writer.writerow(
+            (
+                'Категория',
+                'Анализ',
+                'Биоматериал',
+                'Описание',
+                'Стоимость',
+                'Готовность: дней'
+            )
+        )
+
+        writer.writerows(
+            result
+        )
 
 def main():
     collect_data()
